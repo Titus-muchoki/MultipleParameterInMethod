ec92395149c651d62f57df8dda49a582b22a8e1a
kajela <titoyut56@gmail.com>
2022-06-07 01:36:37 -0400

    add the initial files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,20 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
+    static void myName(String name, int num){
+        System.out.println(name+ "is " +num);
+    }
+    static void myCity(String city, int address){
+        System.out.println(city+ "" +address);
+    }
+    static int myNumber(int y){
+        return y+4;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        myName("tito ",22);
+        myName("kajela",8444);
+        myCity("nairobi ",1824);
+        System.out.println(myNumber(10));
+
     }
 }
